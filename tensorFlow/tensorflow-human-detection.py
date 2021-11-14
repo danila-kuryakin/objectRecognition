041444b7c4d62aa2f8b0371a4780d2ea2d4f77ee
danila-kuryakin <myDanila98git>
2021-11-14 18:39:34 +0300

    add file

---
+++ tensorFlow/tensorflow-human-detection.py
@@ -0,0 +1,89 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+import time
+
+
+class DetectorAPI:
+    def __init__(self, path_to_ckpt):
+        self.path_to_ckpt = path_to_ckpt
+
+        self.detection_graph = tf.compat.v1.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.compat.v1.GraphDef()
+            with tf.io.gfile.GFile(self.path_to_ckpt, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        self.default_graph = self.detection_graph.as_default()
+        self.sess = tf.compat.v1.Session(graph=self.detection_graph)
+
+        # Definite input and output Tensors for detection_graph
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+    def processFrame(self, image):
+        # Expand dimensions since the trained_model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image, axis=0)
+        # Actual detection.
+
+        (boxes, scores, classes, num) = self.sess.run(
+            [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+            feed_dict={self.image_tensor: image_np_expanded})
+
+
+        im_height, im_width, _ = image.shape
+        boxes_list = [None for i in range(boxes.shape[1])]
+        for i in range(boxes.shape[1]):
+            boxes_list[i] = (int(boxes[0, i, 0] * im_height),
+                             int(boxes[0, i, 1] * im_width),
+                             int(boxes[0, i, 2] * im_height),
+                             int(boxes[0, i, 3] * im_width))
+
+        return boxes_list, scores[0].tolist(), [int(x) for x in classes[0].tolist()], int(num[0])
+
+    def close(self):
+        self.sess.close()
+        self.default_graph.close()
+
+if __name__ == "__main__":
+    model_path = 'frozen_inference_graph.pb'
+    odapi = DetectorAPI(path_to_ckpt=model_path)
+    threshold = 0.3
+    cap = cv2.VideoCapture("../resources/image1.jpg")
+    outPath = '../output/TF_image4.jpg'
+
+    while True:
+        r, frame = cap.read()
+        if r:
+            frame = cv2.resize(frame, (800, 600))
+            start_time = time.time()
+            boxes, scores, classes, num = odapi.processFrame(frame)
+            end_time = time.time()
+
+            print("Elapsed Time:", end_time - start_time)
+            # Visualization of the results of a detection.
+
+
+            for i in range(len(boxes)):
+                # Class 1 represents human
+                if classes[i] == 1 and scores[i] > threshold:
+                    box = boxes[i]
+                    cv2.rectangle(frame, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
+
+
+
+            cv2.imshow("TF", frame)
+
+            cv2.imwrite(outPath, frame)
+
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            break
